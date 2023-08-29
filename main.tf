0c2b8b721c7ea5c2081d7fa71334a1a3d469a0b7
Andres Triana <andres.triana@qualitycode.com.co>
2023-08-29 18:29:21 -0500

    Terraform files

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# *****************
+# VPC Configuration
+# *****************
 resource "aws_vpc" "arroyo_rds_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
@@ -8,24 +11,52 @@ resource "aws_internet_gateway" "arroyo_igw" {
   vpc_id = aws_vpc.arroyo_rds_vpc.id
 }
 
+# *********************
+# SUBNETS Configuration
+# *********************
 resource "aws_subnet" "rds_subnet_1" {
   vpc_id     = aws_vpc.arroyo_rds_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
-  map_public_ip_on_launch = true
+  availability_zone = "us-west-2a"
 }
 
 resource "aws_subnet" "rds_subnet_2" {
   vpc_id     = aws_vpc.arroyo_rds_vpc.id
   cidr_block = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
-  map_public_ip_on_launch = true
+  availability_zone = "us-west-2b"
 }
 
 resource "aws_db_subnet_group" "rds_db_subnet_group" {
   name       = "rds-db-subnet-group"
   subnet_ids = [aws_subnet.rds_subnet_1.id, aws_subnet.rds_subnet_2.id]
 }
+
+# *************************
+# RouteTables Configuration
+# *************************
+resource "aws_route_table" "arroyo_route_table" {
+  vpc_id = aws_vpc.arroyo_rds_vpc.id
+}
+
+resource "aws_route" "arroyo_route" {
+  route_table_id         = aws_route_table.arroyo_route_table.id
+  destination_cidr_block = "0.0.0.0/0"  # Ruta por defecto para todo el tráfico
+  gateway_id             = aws_internet_gateway.arroyo_igw.id
+}
+
+resource "aws_route_table_association" "arroyo_subnet_association1" {
+  subnet_id      = aws_subnet.rds_subnet_1.id
+  route_table_id = aws_route_table.arroyo_route_table.id
+}
+
+resource "aws_route_table_association" "arroyo_subnet_association2" {
+  subnet_id      = aws_subnet.rds_subnet_2.id
+  route_table_id = aws_route_table.arroyo_route_table.id
+}
+
+# ***************************
+# SecurityGroup Configuration
+# ***************************
 
 resource "aws_security_group" "rds_sg" {
   name        = "rds_sg"
@@ -38,24 +69,36 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Access from any IP
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
+# ***********************
+# RDS MySql Configuration
+# ***********************
+
 resource "aws_db_instance" "arroyo_rds_mysql" {
-  allocated_storage    = 20
-  storage_type        = "gp2"
   engine              = "mysql"
-  engine_version      = "8.0"
+  identifier          = "arroyomysql"
+  allocated_storage   = 20
+  storage_type        = "gp2"  
+  engine_version      = "8.0.33"
   instance_class      = "db.t2.micro"
-  db_name             = "arroyoDb"
   username            = "admin"
   password            = "Colombia2023."
-  skip_final_snapshot = true
-  
-  
-  publicly_accessible = true
+  parameter_group_name = "default.mysql8.0"  
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot = true
+  publicly_accessible = true
+  
+
   db_subnet_group_name   = aws_db_subnet_group.rds_db_subnet_group.name
-  depends_on = [aws_internet_gateway.arroyo_igw]
+  
+  depends_on = [aws_security_group.rds_sg]
 
   tags = {
     Name = "arroyoDb"
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "rds_endpoint" {
   value = aws_db_instance.arroyo_rds_mysql.endpoint
 }
+
+output "security_group_id" {
+  value = aws_security_group.rds_sg.id
+}
