bce93cf925932896dc4fab3ddcc77ca4c9ca9218
Andres Triana <andrestriana.mct@outlook.com>
2023-08-30 04:27:34 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ variable "region" {
 
 variable "aws_access_key" {
   description = "AWS Credentials"
-  default = "AKIAUM4U53K3KVFE2FNC"
+  default = "XXXXXXXXX"
 }
 
 variable "aws_secret_access_key" {
   description = "AWS Credentials"
-  default = "5405hSaFGAyLL9/HOJ1JWuKZAcVpOS4ifmwe3T7G"
+  default = "XXXXXXXXXX"
 }
